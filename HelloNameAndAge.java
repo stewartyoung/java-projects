4affff2fc5142dbde5cb7b60d36f04e007283d01
Stewart Young <stewartyoung97@gmail.com>
2020-01-12 07:43:41 +0000

    add hello name and age

---
+++ HelloNameAndAge.java
@@ -0,0 +1,6 @@
+public class HelloNameAndAge {
+    public static void main (String[] args){
+        System.out.println("Your name is: " + args[0]);
+        System.out.println("Your age is: " + args[1]);
+    }
+}
