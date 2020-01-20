90e616b5fcd693473f69ae89c9140cd7063852f8
Stewart Young <stewartyoung97@gmail.com>
2020-01-20 18:28:01 +0000

    create jar file from manifest for geometry

---
+++ JarTest.java
@@ -0,0 +1,8 @@
+import geometry.*;
+
+public class JarTest {
+    public static void main(String[] args) {
+        Shape orange = new Circle("circle");
+        System.out.println(orange);
+    }
+}
